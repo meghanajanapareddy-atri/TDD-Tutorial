842ad76c17f1e427282776b01aa33e65d18ab21b
Meghana Janapareddy <janapare@atrihub.io>
2023-06-02 14:08:45 -0700

    feat(training):TDD Tutorial Chapter 6
    Regression Test
    
    ICTRAINING-1506

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
+from unittest import TestCase
+
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import WebDriverException
+
+from lists.models import Item
 
 MAX_WAIT = 10
 
@@ -122,4 +126,20 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
